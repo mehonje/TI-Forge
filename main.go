fa8808182562b6f73938a6b1fc8007362dd5a632
mehonje <mehonje@gmail.com>
2026-06-04 14:32:37 -0500

    Add better rendering of converted 8xp data (main.go) Includes line numbers

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"ti_forge/convert"
 )
 
 func main() {
-	var program_data []byte = convert.Read8xp("C:/Users/Jack/Onedrive/Desktop/go_work/ti_forge/TEST.8xp")
+	var program_data []byte = convert.Read8xp("C:/Users/Jack/Onedrive/Desktop/go_work/ti_forge/TEST3.8xp")
 	display_data(program_data)
 }
 
-func display_data(program_data []byte) {
-	fmt.Println(convert.Data_to_strings(program_data))
+func display_data(program_data_bytes []byte) {
+	program_data_strings := convert.Data_to_strings(program_data_bytes)
+	max_line_num_len := len(strconv.Itoa(len(program_data_strings)))
+	line_num_fmtstr := fmt.Sprintf("%%%ds %%s", max_line_num_len)
+
+	var builder strings.Builder
+	builder.Grow(len(program_data_strings) * (max_line_num_len + 1 + 40))
+
+	for i, line := range program_data_strings {
+		fmt.Fprintf(&builder, line_num_fmtstr, strconv.Itoa(i), line)
+	}
+	fmt.Print(builder.String())
 }
