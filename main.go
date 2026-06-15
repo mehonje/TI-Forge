180cce4e254a80760462c75064c3dbbdcbcb39dc
mehonje <mehonje@gmail.com>
2026-06-15 14:56:58 -0500

    Change how user input works (main.go)
    User input is now processed as runes (and bytes for non-character keys) instead of bytes

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-	"golang.org/x/term"
 	"ti_forge/convert"
 	"unicode/utf8"
-)
 
-var cursor_row int = 1
-var cursor_col int = 1
+	"golang.org/x/term"
+)
 
 func main() {
 	enable_utf8()
+
+	var cursor_row int = 1
+	var cursor_col int = 1
+	var text_buffer []rune = []rune{}
+	var mode int = 0
 	
 	fd := int(os.Stdin.Fd())
 	cooked_state, err := term.MakeRaw(fd)
@@ -28,30 +31,30 @@ func main() {
 	var program_data []byte = convert.Read8xp("C:/Users/Jack/Onedrive/Desktop/go_work/ti_forge/TEST3.8xp")
 	program_data_commands := convert.Data_to_strings(program_data)
 	for {
-		display_data(program_data_commands, cursor_row, cursor_col)
-		switch process_input(get_input()) {
-			case 1: return
+		cursor_row, cursor_col = display_data(program_data_commands, cursor_row, cursor_col, text_buffer, mode)
+
+		var quit bool = false
+		quit, text_buffer, cursor_row, cursor_col = process_input(get_input(), cursor_row, cursor_col, text_buffer)
+		
+		if quit {
+			return
 		}
 	}
 }
 
-func display_data(program_data_lines [][]string, input_cursor_row int, input_cursor_col int) {
-	if input_cursor_row < 1 {
+func display_data(program_data_lines [][]string, cursor_row int, cursor_col int, text_buffer []rune, mode int) (int, int) {
+	if cursor_row < 1 {
 		cursor_row = 1
-		input_cursor_row = 1
 	}
-	if input_cursor_row > len(program_data_lines) {
+	if cursor_row > len(program_data_lines) {
 		cursor_row = len(program_data_lines)
-		input_cursor_row = cursor_row
 	}
-	if input_cursor_col < 1 {
+	if cursor_col < 1 {
 		cursor_col = 1
-		input_cursor_col = 1
 	}
 	var line_length int = len(program_data_lines[cursor_row-1])
-	if input_cursor_col > line_length {
+	if cursor_col > line_length {
 		cursor_col = line_length
-		input_cursor_col = cursor_col
 	}
 
 	max_line_num_len := len(strconv.Itoa(len(program_data_lines)))
@@ -60,6 +63,7 @@ func display_data(program_data_lines [][]string, input_cursor_row int, input_cur
 	fmt.Print("\033[H\033[2J")
 
 	_, height := get_term_size()
+	height--
 	half_height := height/2
 	var buffer_row int = cursor_row-1
 	var buffer_start int = max(0, buffer_row-half_height)
@@ -75,6 +79,12 @@ func display_data(program_data_lines [][]string, input_cursor_row int, input_cur
 		fmt.Fprintf(&builder, line_num_fmtstr, strconv.Itoa(i+1),line_builder.String())
 	}
 	fmt.Print(builder.String())
+	var mode_string string = ""
+	switch mode {
+	case 0:
+		mode_string = "NORMAL"
+	}
+	fmt.Print(mode_string, "   ", string(text_buffer))
 
 	var screen_row int = buffer_row-buffer_start
 	var screen_col int = max_line_num_len+2
@@ -83,6 +93,8 @@ func display_data(program_data_lines [][]string, input_cursor_row int, input_cur
 	}
 
 	fmt.Printf("\033[%d;%dH", screen_row+1, screen_col) // move cursor
+	
+	return cursor_row, cursor_col
 }
 
 func get_input() byte {
@@ -94,20 +106,30 @@ func get_input() byte {
     return buf[0]
 }
 
-func process_input(input byte) int {
+func process_input(input byte, cursor_row int, cursor_col int, text_buffer []rune) (bool, []rune, int, int) {
+	var quit bool = false
+	var new_text_buffer []rune = text_buffer
+
 	switch input {
-	case 104: // Left
-		cursor_col--
-	case 108: // Right
-		cursor_col++
-	case 106: // Down
-		cursor_row++
-	case 107: // Up
-		cursor_row--
 	case 113: // "q"
-		return 1
+		quit = true
 	}
-	return 0
+
+	var rune_input rune = rune(input)
+	switch rune_input {
+	case 'h':
+		cursor_col--
+	case 'l':
+		cursor_col++
+	case 'j':
+		cursor_row++
+	case 'k':
+		cursor_row--
+	default:
+		new_text_buffer = append(new_text_buffer, rune_input)
+	}
+
+	return quit, new_text_buffer, cursor_row, cursor_col
 }
 
 func get_term_size() (int, int) {
