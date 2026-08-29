6c5c33729bef967b52bb8d1b31afba520bc4ad60
mehonje <mehonje@proton.me>
2026-08-29 17:11:59 -0500

    Add Text_color and Background_color functions (ansi/ansi.go) Uses ANSI codes to set RGB colors

--- ansi/ansi.go
+++ ansi/ansi.go
@@ -1,12 +1,22 @@
 package ansi
 
+import "fmt"
+
 const (
-	Clear string = "\033[2J"
+	Clear        string = "\033[2J"
 	Reset_cursor string = "\033[H"
-	Highlight string = "\033[7m"
-	Reset_text string = "\033[0m"
-	Show_cursor string = "\033[?25h"
-	Hide_cursor string = "\033[?25l"
-	Red string = "\033[31m"
-	Bold string = "\033[1m"
+	Highlight    string = "\033[7m"
+	Reset_text   string = "\033[0m"
+	Show_cursor  string = "\033[?25h"
+	Hide_cursor  string = "\033[?25l"
+	Red          string = "\033[31m"
+	Bold         string = "\033[1m"
 )
+
+func Text_color(r int, g int, b int) string {
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b)
+}
+
+func Background_color(r int, g int, b int) string {
+	return fmt.Sprintf("\033[48;2;%d;%d;%dm", r, g, b)
+}
